3a435976f4ece8f0bfc0674df983e166ef7f8ceb
BalaBaskarfrl <bala.b@focusresearchlabs.com>
2020-07-16 20:21:28 +0530

    Revert "included processors"
    This reverts commit e35271f8c5809b1c0240e5d61ee5ed1ba1657eb0.

--- src/config/config.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.configDB = void 0;
-exports.configDB = "mongodb://localhost:27017/geo-admin";
-//# sourceMappingURL=config.js.map
