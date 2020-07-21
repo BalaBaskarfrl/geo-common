8ca99ebf911fcf8e8d981af5bc435584f4d3181e
BalaBaskarfrl <bala.b@focusresearchlabs.com>
2020-07-21 13:35:27 +0530

    Git updated

--- dist/index.js
+++ dist/index.js
@@ -1,88 +1,15 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !exports.hasOwnProperty(p)) __createBinding(exports, m, p);
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.ruleService = void 0;
-const GeoUserEventEntity_1 = require("./src/data-access/GeoUserEventEntity");
 //server\vendor\data-access\FRLEventEntity.ts
 //import APIError from './APIError';
-class FrlProcessor {
-    constructor() {
-        this.preProcessor = (callback, args, name, type) => __awaiter(this, void 0, void 0, function* () {
-            const geoEvents = new GeoUserEventEntity_1.GEOEventEntity();
-            let result;
-            //insert request.body into `geouserevents`
-            const preProcessorEvent = yield geoEvents.insert(args[0]);
-            console.log(preProcessorEvent);
-            this.userEventId = preProcessorEvent["_id"];
-            let payload = getNested(args[0], "role", "request");
-            args[0] = (typeof (payload) !== "undefined") ? payload : args[0];
-            args[0]["userEventId "] = this.userEventId;
-            result = callback();
-            return result;
-        });
-        this.postProcessor = (callback, args, name, type) => __awaiter(this, void 0, void 0, function* () {
-            let result;
-            result = yield callback();
-            const geoEvents = new GeoUserEventEntity_1.GEOEventEntity();
-            if (result.hasOwnProperty("success") && result.success == true)
-                return result;
-            else {
-                //update only error message into `geouserevents`
-                const postProcessorEvent = yield geoEvents.updateEventResponseById(this.userEventId, result);
-                return "Error";
-            }
-            ;
-        });
-        this.userEventId = null;
-    }
-}
-exports.default = FrlProcessor;
-function getNested(obj, ...args) {
-    return args.reduce((obj, level) => obj && obj[level], obj);
-}
-function ruleService(wrapperMethod) {
-    return (target, key, descriptor) => {
-        if (typeof (target) === 'function') {
-            let newTarget = function (...arg) {
-                return __awaiter(this, void 0, void 0, function* () {
-                    var self = this;
-                    return function () {
-                        return __awaiter(this, void 0, void 0, function* () {
-                            var methodCallback = function () {
-                                return __awaiter(this, void 0, void 0, function* () { return new target(arg); });
-                            };
-                            return yield wrapperMethod.call(self, methodCallback, arg, target.name, 'class');
-                        });
-                    }();
-                });
-            };
-            return newTarget;
-        }
-        else {
-            let orgMethod = descriptor.value;
-            descriptor.value = function (...arg) {
-                return __awaiter(this, void 0, void 0, function* () {
-                    var self = this;
-                    return function () {
-                        return __awaiter(this, void 0, void 0, function* () {
-                            var methodCallback = function () {
-                                return __awaiter(this, void 0, void 0, function* () { return yield orgMethod.apply(self, arg); });
-                            };
-                            return yield wrapperMethod.call(self, methodCallback, arg, key, 'function');
-                        });
-                    }();
-                });
-            };
-            return descriptor;
-        }
-    };
-}
-exports.ruleService = ruleService;
+__exportStar(require("./src/controller/rulesProcessor"), exports);
---
+++ src/controller/rulesProcessor.ts
@@ -0,0 +1,68 @@
+import {GEOEventEntity}  from '../data-access/GeoUserEventEntity';
+//server\vendor\data-access\FRLEventEntity.ts
+//import APIError from './APIError';
+export default class FrlProcessor{
+    userEventId:any;
+    constructor() {
+        this.userEventId  =   null;
+      }
+    preProcessor  =   async (callback:any, args:any, name:string, type:string)=>{
+
+
+        const geoEvents         =   new GEOEventEntity();
+        let result: any;
+        //insert request.body into `geouserevents`
+        
+        const preProcessorEvent = await geoEvents.insert(args[0]); 
+        console.log(preProcessorEvent)
+        this.userEventId        =   preProcessorEvent["_id"];
+        let payload             =   getNested(args[0],"role","request");
+        args[0]                 = (typeof(payload)!=="undefined")?payload:args[0];  
+        args[0]["userEventId "] =   this.userEventId
+        result =  callback();
+        return result;
+    };
+    postProcessor  = async  (callback, args, name, type)=>{
+        let result: any;
+        result              =  await callback();
+
+        const geoEvents     =   new GEOEventEntity();
+
+        if(result.hasOwnProperty("success") && result.success==true ) return result;
+
+        else {
+            //update only error message into `geouserevents`
+            const postProcessorEvent = await geoEvents.updateEventResponseById(this.userEventId,result);
+            return "Error";
+        };
+
+    };
+}
+function getNested(obj, ...args) {
+    return args.reduce((obj, level) => obj && obj[level], obj)
+}
+export  function ruleService(wrapperMethod:any) {
+    return (target, key, descriptor) => {
+  
+      if ( typeof(target) === 'function' ){
+        let newTarget = async function (...arg) { 
+          var self = this;
+          return async function() {
+            var methodCallback = async function(){return new target(arg)};
+            return await wrapperMethod.call(self, methodCallback, arg, target.name, 'class')
+          }()
+        };
+        return newTarget;
+      } else {
+        let orgMethod = descriptor.value;
+        descriptor.value = async function (...arg) {
+          var self = this;
+          return async function() {
+            var methodCallback = async function() { return await orgMethod.apply(self, arg) };
+            return await wrapperMethod.call(self, methodCallback, arg, key, 'function')
+          }()
+        };
+        return descriptor;
+      }
+    }
+  }
